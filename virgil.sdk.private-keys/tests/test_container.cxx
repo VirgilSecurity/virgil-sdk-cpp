166ce09f70f631aa12bd464a48de6b85ae842ef0
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2015-09-09 13:32:14 +0300

    [PKSDK] [Tests] Make unit tests empty for Private Keys SDK.

--- virgil.sdk.private-keys/tests/test_container.cxx
+++ virgil.sdk.private-keys/tests/test_container.cxx
@@ -6,34 +6,8 @@
 
 #include "fakeit.hpp"
 
-#include <virgil/sdk/keys/client/KeysClient.h>
-#include <virgil/sdk/keys/model/ContainerType.h>
-#include <virgil/sdk/keys/util/JsonKey.h>
-
 using json = nlohmann::json;
 using namespace fakeit;
 
-using virgil::sdk::keys::client::KeysClientConnection;
-using virgil::sdk::keys::client::KeysClient;
-using virgil::sdk::keys::model::ContainerType;
-using virgil::sdk::keys::util::JsonKey;
-
-
-ContainerType foo();
-
-
-TEST_CASE("Container foo", "normal") {
-
-
-    ContainerType t = foo();
-
-
+TEST_CASE("Empty test", "[virgil-sdk-private-keys]") {
 }
-
-ContainerType foo() {
-    json containerTypeJson =  { JsonKey::containerType, "normal" };
-    std::string containerTypeStr = containerTypeJson[1];
-    return containerTypeStr == "easy" ? ContainerType::kEasy : ContainerType::kNormal;
-}
-
-
