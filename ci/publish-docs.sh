503e3d9d944760a63f707ebc77b256b8ac6af39c
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2015-12-01 17:21:50 +0200

    [All] [CI] Fix link to the publish docs repo after it was renaming.

--- ci/publish-docs.sh
+++ ci/publish-docs.sh
@@ -40,7 +40,7 @@ set -ev
 if [ "${PUBLISH_DOCS}" != "ON" ]; then exit; fi
 
 # Settings
-REPO_PATH=git@github.com:VirgilSecurity/virgil-cpp.git
+REPO_PATH=git@github.com:VirgilSecurity/virgil-sdk-cpp.git
 HTML_PATH_DST="${TRAVIS_BUILD_DIR}/${BUILD_DIR_NAME}/docs/html"
 COMMIT_USER="Travis CI documentation builder."
 COMMIT_EMAIL="sergey.seroshtan@gmail.com"
