ffbeda37a30f89e1e269319fa16f03e6f0e29337
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2016-02-19 12:23:16 +0200

    [CPPSDK] Add documentation to class 'IdentityClient'

--- include/virgil/sdk/client/IdentityClient.h
+++ include/virgil/sdk/client/IdentityClient.h
@@ -41,7 +41,6 @@
 
 #include <virgil/sdk/model/ValidatedIdentity.h>
 #include <virgil/sdk/model/Identity.h>
-#include <virgil/sdk/model/Card.h>
 #include <virgil/sdk/http/Request.h>
 #include <virgil/sdk/http/Response.h>
 
@@ -49,18 +48,50 @@ namespace virgil {
 namespace sdk {
     namespace client {
         /**
-         * @brief Entrypoint for interacting with Identity Service PKI.
+         * @brief Entrypoint for interacting with Virgil Identity Service
+         *
+         * Virgil Identity Service is responsible for validation of user's identities,
+         * like email, application, etc. Typical workflow for an email confirmation contains the following steps:
+         *   - The process is initiated by invocation of the verify() method
+         *   - Identity service generates a confirmation code and sends it to the specified email
+         *   - Confirm email by invoke method @link confirm() @endlink with
+         *         the confirmation code sent to the email and identity id from the previous step
+         *   - Identity service returns the token within class @link virgil::sdk::model::ValidatedIdentity @endlink,
+         *         that can be used to prove that the user is the identity holder
+         *   - To verify that the user is identity holder invoke the @link isValid() @endlink method
+         *         with the token from the previous step
          */
         class IdentityClient : public Client {
         public:
             using Client::Client;
-
+            /**
+             * @brief Initiate identity verification process
+             * @param identity - user's identity, that is sent to verification
+             * @return Unique identifier of the initiated verification action
+             */
             std::string verify(const virgil::sdk::model::Identity& identity);
-
+            /**
+             * @brief [brief description]
+             * @details [long description]
+             *
+             * @param actionId - unique identifier that was returned by @link verify() @endlink method
+             * @param confirmationCode - unique code, that was sent to the given identity
+             * @param timeToLive - limit the lifetime of the token in seconds,
+             *                     maximum value is 60 * 60 * 24 * 365 = 1 year,
+             *                     default value is 3600 = 1 hour
+             * @param countToLive - limit token usage count,
+             *                      default value is 1 which means that the token can be used at most one time
+             * @return Identity with validation token inside
+             */
             virgil::sdk::model::ValidatedIdentity confirm(const std::string& actionId,
                                                           const std::string& confirmationCode,
                                                           const int timeToLive = 3600, const int countToLive = 1);
-
+            /**
+             * @brief Validate given token
+             *
+             * @param validatedIdentity - identity and token to be validated
+             * @return true if given token is valid, false - otherwise
+             */
             bool isValid(const virgil::sdk::model::ValidatedIdentity& validatedIdentity);
         };
     }
