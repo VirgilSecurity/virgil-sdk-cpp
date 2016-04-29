eed4969d327ec5fa0516a5d13a6a097f95aca266
VladEvka <vladislav.evka@gmail.com>
2016-04-29 10:32:39 +0300

    [CPPSDK] Add class Obfuscator
    Derives the obfuscated data from incoming parameters using PBKDF function.

---
+++ src/virgil/sdk/util/Obfuscator.cxx
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2015 Virgil Security Inc.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <virgil/crypto/VirgilByteArray.h>
+#include <virgil/crypto/foundation/VirgilPBKDF.h>
+#include <virgil/crypto/foundation/VirgilBase64.h>
+
+#include <virgil/sdk/util/Obfuscator.h>
+
+using virgil::crypto::VirgilByteArray;
+using virgil::crypto::foundation::VirgilPBKDF;
+using virgil::crypto::foundation::VirgilBase64;
+
+using virgil::sdk::util::Obfuscator;
+
+std::string Obfuscator::process(const std::string& value, const std::string& salt, const VirgilPBKDF::Hash& algorithm,
+                   const unsigned int iterations) {
+    VirgilPBKDF pbkdf(virgil::crypto::str2bytes(salt), iterations);
+    pbkdf.setHash(algorithm);
+    VirgilByteArray sequence = pbkdf.derive(virgil::crypto::str2bytes(value));
+    return VirgilBase64::encode(sequence);
+}
