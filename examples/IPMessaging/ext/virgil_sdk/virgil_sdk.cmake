89fa39b501664c0503ad09bf753df296da986f04
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2016-05-12 08:31:43 +0300

    [SDK-67] [Examples] Apply 'virgil_depends' module on the 'IPMessaging' example

--- examples/IPMessaging/ext/virgil_sdk/virgil_sdk.cmake
+++ examples/IPMessaging/ext/virgil_sdk/virgil_sdk.cmake
@@ -34,33 +34,27 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-# Dependecy to https://github.com/nlohmann/json
+cmake_minimum_required (VERSION @CMAKE_VERSION@ FATAL_ERROR)
 
-# Configure external project
-if (NOT TARGET project_json)
-    ExternalProject_Add (project_json
-        GIT_REPOSITORY "https://github.com/nlohmann/json.git"
-        GIT_TAG "v1.1.0"
-        PREFIX "${CMAKE_BINARY_DIR}/ext/json"
-        SOURCE_DIR "${CMAKE_BINARY_DIR}/ext/json/src/project_json"
-        CMAKE_COMMAND ""
-        BUILD_COMMAND ""
-        INSTALL_COMMAND ""
-        TEST_COMMAND ""
-    )
-endif ()
+project ("@VIRGIL_DEPENDS_PACKAGE_NAME@-depends")
 
-if (NOT TARGET json)
-    # Configure output
-    set (JSON_INCLUDE_DIRS "${CMAKE_BINARY_DIR}/ext/json/src/project_json/src")
+include (ExternalProject)
 
-    # Workaround of http://public.kitware.com/Bug/view.php?id=14495
-    file (MAKE_DIRECTORY ${JSON_INCLUDE_DIRS})
+# Configure additional CMake parameters
+file (APPEND "@VIRGIL_DEPENDS_ARGS_FILE@"
+    "set (ENABLE_TESTING OFF CACHE INTERNAL \"\")\n"
+    "set (ENABLE_EXAMPLES OFF CACHE INTERNAL \"\")\n"
+    "set (INSTALL_EXT_LIBS @INSTALL_EXT_LIBS@ CACHE INTERNAL \"\")\n"
+    "set (INSTALL_EXT_HEADERS @INSTALL_EXT_HEADERS@ CACHE INTERNAL \"\")\n"
+)
 
-    # Make target
-    add_library (json STATIC IMPORTED GLOBAL)
-    set_target_properties (json PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${JSON_INCLUDE_DIRS}
-    )
-    add_dependencies (json project_json)
-endif ()
+ExternalProject_Add (${PROJECT_NAME}
+    DOWNLOAD_DIR "@VIRGIL_DEPENDS_PACKAGE_DOWNLOAD_DIR@"
+    URL "https://github.com/VirgilSecurity/virgil-sdk-cpp/archive/e732816f8deec5d634c7a8c073da20586e5ae2f0.tar.gz"
+    URL_HASH SHA1=66f78a3ccc5e93c708091da4949af2614fb7e89a
+    PREFIX "@VIRGIL_DEPENDS_PACKAGE_BUILD_DIR@"
+    CMAKE_ARGS "@VIRGIL_DEPENDS_CMAKE_ARGS@"
+)
+
+add_custom_target ("${PROJECT_NAME}-build" ALL COMMENT "Build package ${PROJECT_NAME}")
+add_dependencies ("${PROJECT_NAME}-build" ${PROJECT_NAME})
