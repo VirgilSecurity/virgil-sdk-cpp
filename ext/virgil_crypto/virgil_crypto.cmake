e732816f8deec5d634c7a8c073da20586e5ae2f0
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2016-05-12 08:03:26 +0300

    [SDK-67] Update 'virgil_depends' module

--- ext/virgil_crypto/virgil_crypto.cmake
+++ ext/virgil_crypto/virgil_crypto.cmake
@@ -52,8 +52,8 @@ file (APPEND "@VIRGIL_DEPENDS_ARGS_FILE@"
 
 ExternalProject_Add (${PROJECT_NAME}
     DOWNLOAD_DIR "@VIRGIL_DEPENDS_PACKAGE_DOWNLOAD_DIR@"
-    URL "https://github.com/VirgilSecurity/virgil-crypto/archive/031ecf12aadc44bed02a255265f62f12ce1b56fa.tar.gz"
-    URL_HASH SHA1=8fdc6761f3e7425b4c54a64355478d7867cd14b7
+    URL "https://github.com/VirgilSecurity/virgil-crypto/archive/v1.6.0.tar.gz"
+    URL_HASH SHA1=ec12bdeff28371ad069fe35048266442c98d451e
     PREFIX "@VIRGIL_DEPENDS_PACKAGE_BUILD_DIR@"
     CMAKE_ARGS "@VIRGIL_DEPENDS_CMAKE_ARGS@"
 )
