a871491c60d3835d96563646613b88760c289224
Oleksandr Deundiak <Deundiak@gmail.com>
2016-11-15 16:21:03 +0200

    Updated crypto dependency to version 2.0.2

--- ext/virgil_crypto/virgil_crypto.cmake
+++ ext/virgil_crypto/virgil_crypto.cmake
@@ -52,8 +52,8 @@ file (APPEND "@VIRGIL_DEPENDS_ARGS_FILE@"
 
 ExternalProject_Add (${PROJECT_NAME}
     DOWNLOAD_DIR "@VIRGIL_DEPENDS_PACKAGE_DOWNLOAD_DIR@"
-    URL "https://github.com/VirgilSecurity/virgil-crypto/archive/v1.8.1.tar.gz"
-    URL_HASH SHA1=4cf43a2eebf49645b5dbe2cff8c85ef8e6e95a1c
+    URL "https://github.com/VirgilSecurity/virgil-crypto/archive/v2.0.2.tar.gz"
+    URL_HASH SHA1=e10d66656c6aac67a02293099460b2064f8bc4df
     PREFIX "@VIRGIL_DEPENDS_PACKAGE_BUILD_DIR@"
     CMAKE_ARGS "@VIRGIL_DEPENDS_CMAKE_ARGS@"
 )
